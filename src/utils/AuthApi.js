2c0b66680d48a8bb72c2febb71919592ce3de61c
Aldonfirst <alchevck44@bk.ru>
2023-10-04 22:50:31 +0300

    дополнения в readme

--- src/utils/AuthApi.js
+++ src/utils/AuthApi.js
@@ -1,5 +1,5 @@
-// const BASE_URL = "http://localhost:3000";
-const BASE_URL = 'https://aldonmovie.nomoredoma.nomoredomainsicu.ru';
+const BASE_URL = "http://localhost:3000";
+// const BASE_URL = 'https://aldonmovie.nomoredoma.nomoredomainsicu.ru';
 
 const checkResponseStatus = async (response) => {
   if (response.ok) {
